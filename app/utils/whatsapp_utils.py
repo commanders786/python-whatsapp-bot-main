1311e445953c677256e11759ccc3ed075366cb41
Ashhad Muhammed Kader <93571194+ashhadmuhammed@users.noreply.github.com>
2025-07-20 01:32:22 +0530

    added  changes for closed shop

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -106,16 +106,16 @@ def process_whatsapp_message(body):
         start_time = time(7, 0, 0)   # 7:00 AM
         end_time = time(22, 0, 0)    # 8:00 PM
         
-        # if now < start_time or now > end_time :
-        if True:
+        if now < start_time or now > end_time :
+        # if True:
 
            if message["type"] == "interactive" and "button_reply" in message["interactive"]:
                 if message["interactive"]["button_reply"]["id"]  in ['en','ml']:
                   pass
            else:
                 
-                # response ="സ്റ്റോർ അടച്ചിരിക്കുന്നു. ദയവായി രാവിലെ 7 മണി മുതൽ രാത്രി 8 മണി വരെ ഷോപ്പിംഗ് ശ്രമിക്കുക.\nCall +919961575781 "
-                response="ഇന്ന് സ്റ്റോർ അവധി ആൺ \nCall +919961575781 "
+                response ="സ്റ്റോർ അടച്ചിരിക്കുന്നു. ദയവായി രാവിലെ 7 മണി മുതൽ രാത്രി 8 മണി വരെ ഷോപ്പിംഗ് ശ്രമിക്കുക.\nCall +919961575781 "
+                # response="ഇന്ന് സ്റ്റോർ അവധി ആൺ \nCall +919961575781 "
                 data = get_text_message_input(wa_id, response)
                 send_message(data)
                 return
